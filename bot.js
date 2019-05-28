df878ee0eb65e3b8d7532b4cbf47114be3e6dfe4
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-05-28 18:10:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -398,30 +398,14 @@ client.on('message', (message) => {
     }
 });
 
-client.on('message', message => {
-
-  if (message.author.bot) return;
-
-  if (!message.content.startsWith(prefix)) return;
-
-
-  let command = message.content.split(" ")[0];
-
-  command = command.slice(prefix.length);
-
-
-  let args = message.content.split(" ").slice(1);
-
-
-// -say
-
- if (command === "say") {
-
-          message.delete()
-
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-
-  }
+client.on('message',function(message) {
+	let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); 
+}
+});
 
 
   
