939ef2650db8af23c1f9ac866643c700ca8d0e3d
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-05-28 18:06:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -397,6 +397,64 @@ client.on('message', (message) => {
         channel.delete()
     }
 });
+
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+ if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+
+  
+
+ 
+
+
+if (command == "embed") {
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.sendMessage("**You don't have permission ADMINISTAOR**").then(m => m.delete(5000));
+    let say = new Discord.RichEmbed()
+
+    .setDescription(args.join(" "))
+
+    .setColor(0x23b2d6)
+
+    message.channel.sendEmbed(say);
+
+    message.delete();
+
+  }
+
+
+
+});
+
+
+client.on("ready", () => {
+  function lol() {
+    client.guilds.get('id server').roles.find("name", "Rainbow").setColor("RANDOM");
+  }
+  setInterval(lol, 6000);
+})
+
   
   
 client.on('message', message => {
